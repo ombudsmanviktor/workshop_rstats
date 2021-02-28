dc53277dd7e0693c9823281e8e59c90d853d44a6
Viktor Chagas <30334674+ombudsmanviktor@users.noreply.github.com>
2021-02-28 15:19:26 -0300

    Add files via upload

---
+++ programa_render.R
@@ -0,0 +1,6 @@
+#install.packages("rmarkdown")
+#install.packages("prettydoc")
+library("rmarkdown")
+library("prettydoc")
+
+rmarkdown::render("/Volumes/HD VIKTOR/aulas e seminários/disciplinas/GEC Pesquisa Quantitativa/programa.Rmd")
