1428a709d3e988fca4228a227ee2a463ce9e4045
Viktor Chagas <30334674+ombudsmanviktor@users.noreply.github.com>
2021-04-05 19:23:41 -0300

    Add files via upload

--- aula9/revisao2.R
+++ aula9/revisao2.R
@@ -1,6 +1,26 @@
-install.packages("rtweet")
+# Curso: Introdução à Análise de Pesquisa Quantitativa
+# Professor: Viktor Chagas
+# Semestre Letivo: 2020.2
+# Aula: 9
+# Programa de curso: https://rpubs.com/viktor/workshop_rstats
+
+# Esta é uma aula apenas de revisão. Seu objetivo é treinar e fixar algumas funções do pacote
+# rtweet. O script abaixo serve como guia para os tutoriais em vídeo disponíveis nos
+# seguintes links:
+
+# Coleta de dados com rtweet: https://classroom.google.com/c/MjYxMjQ1MjQ3MDYy/p/MzIxMDU2OTU1MDQw/details
+
+# Coleta de dados com rtweet + análise exploratória com ggplot2: https://classroom.google.com/c/MjYxMjQ1MjQ3MDYy/p/MzEyNzg4MDU2NDE5/details
+
+# Coleta de dados com rtweet + análise de redes com igraph: https://classroom.google.com/c/MjYxMjQ1MjQ3MDYy/p/MzEyNzg4MDU2NDMw/details
+
+# Requisição de pacotes
+
+#install.packages("rtweet")
 library(rtweet)
 library(dplyr)
+
+# Autenticação
 
 api_key <- "x7dRwr3tRI7oOBeDIaKta49Ga"
 api_secret_key <- "jicCkpplw7eBEi8kLISgAxFllHgoUuRKYObFbggSaVPnCNPpFU"
@@ -15,6 +35,8 @@ token <- create_token(
   access_secret = access_token_secret
 )
 
+# Coleta de dados 1
+
 bolsonaro <- stream_tweets(
   "bolsonaro", timeout = 60
 )
@@ -23,6 +45,8 @@ rodolffo <- search_tweets(
   "#ForaRodolffo", n = 200, retryonratelimit = FALSE, verbose = TRUE, include_rts = FALSE
 )
 
+# Análise exploratória 1
+
 arrobas <- rodolffo %>% 
   group_by(screen_name) %>% 
   summarise(screen_name) %>% 
@@ -30,7 +54,7 @@ arrobas <- rodolffo %>%
   arrange(desc(n)) %>% 
   head(10) %>% 
   ggplot2::ggplot(ggplot2::aes(x = screen_name, y = n)) +
-    ggplot2::geom_bar(stat = "identity", width = .9)
+  ggplot2::geom_bar(stat = "identity", width = .9)
 plot(arrobas)
 
 rodolffo %>% 
@@ -39,9 +63,13 @@ rodolffo %>%
   ggplot2::ggplot(ggplot2::aes(x = min, y = n)) +
   ggplot2::geom_line(stat = "identity")
 
+# Coleta de dados 2
+
 viih <- search_tweets(
   "#ForaViihTube", n = 10000, include_rts = FALSE, verbose = TRUE
 )
+
+# Análise exploratória 2
 
 viih %>% 
   mutate(tempo = lubridate::floor_date(created_at, "minute")) %>% 
@@ -60,7 +88,7 @@ tweets_bbb <- rodolffo %>%
   select(user_id, status_id, text) %>% 
   full_join(viih)
 
-#
+# Análise de rede
 
 rede_seguidores <- get_followers(
   "museudememes", n = 100, parse = TRUE
@@ -87,7 +115,3 @@ grafo <- igraph::graph_from_data_frame(d = edges,
                                        vertices = nodes)
 
 plot(grafo, vertex.label = NA, layout = layout_nicely(grafo))
-
-
-
-
